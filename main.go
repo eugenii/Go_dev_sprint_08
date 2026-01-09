5be3c8737dc8255728dab10a9cd83d4a3b15b7ba
eugenii <eugenii.cher@gmail.com>
2026-01-09 11:51:23 +0300

    return changes to develop

--- main.go
+++ main.go
@@ -98,8 +98,13 @@ func (s ParcelService) Delete(number int) error {
 
 func main() {
 	// настройка подключения к БД
+	db, err := sql.Open("sqlite", "tracker.db")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
-	store := // создайте объект ParcelStore функцией NewParcelStore
+	store := NewParcelStore(db) // Oбъект ParcelStore
 	service := NewParcelService(store)
 
 	// регистрация посылки
